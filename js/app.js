f4dd189b5d1ecd8f3330feb5351c2be4e69ab1ec
DorzelRamos <dorzel.ramos@gmail.com>
2022-10-26 11:25:33 -0500

    added forloop

--- js/app.js
+++ js/app.js
@@ -21,7 +21,21 @@ let color3 = "linear-gradient(35deg, #0B132B 0%, #5BC0BE 100%)";
 let color4 = "linear-gradient(235deg, #5BC0BE 0%, #3A506B 100%)";
 
 let colorsArray = [color1, color2, color3, color4]
-console.log(colorsArray);
+//console.log(colorsArray);
 
 let wrapper_Divs = document.getElementsByClassName('wrapperDivs');
-console.log(wrapper_Divs);
+//console.log(wrapper_Divs);
+
+let divOne = wrapper_Divs.item(0);
+let divTwo = wrapper_Divs.item(1);
+let divThree = wrapper_Divs.item(2);
+let divFour = wrapper_Divs.item(3);
+
+let allDivs = [divOne, divTwo, divThree, divFour];
+//console.log(allDivs);
+
+for (let i=0; i < 4; i++) {
+    let myDiv = allDivs[i];
+    myDiv.style.backgroundImage = colorsArray[i];
+    //above is whats called a javaScript CSS. Its weird 
+}
